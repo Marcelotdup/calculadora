c0d51c74548e06e28b0b9cde1fe9c39e2794f8d0
Marcelo Dupont <marcelotdupont@gmail.com>
2023-05-31 21:43:23 -0300

    primeiro commit - calculadora

--- src/main/java/calculadora/Principal.java
+++ src/main/java/calculadora/Principal.java
@@ -1,14 +1,17 @@
 package calculadora;
+
 import javax.swing.JOptionPane;
 import calculadora.Calculadora;
+
 public class Principal {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String opcao = "";
         Calculadora calculadora = new Calculadora();
-        while (!opcao.equals("9")){
+        while (!opcao.equals("9")) {
             opcao = JOptionPane.showInputDialog("1 - Leitura \n2- Soma \n3- Subtração "
                     + "\n4- Multiplicação \n5- Divisão \n9- Sair");
-            switch (Integer.parseInt(opcao)){
+            switch (Integer.parseInt(opcao)) {
                 case 1:
                     calculadora.setValorA(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de A:")));
                     calculadora.setValorB(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de B:")));
